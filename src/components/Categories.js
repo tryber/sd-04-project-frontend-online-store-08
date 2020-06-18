b09cfaac50f307387cc7c0656ae5fe53bd5d692e
Leandro Marten <leandromarten1@gmail.com>
2020-06-18 16:14:18 -0300

    all categories listed

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import * as api from '../services/api'; // importando as funções da API como api
 
 class Categories extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { categories: [] };
+  }
+
+  componentDidMount() {
+    api
+      .getCategories()
+      .then((categories) =>
+        this.setState((state) => ({ ...state, categories })),
+      );
+  }
 
   render() {
+    const { categories } = this.state;
     return (
       <div className="categories">
         <h4>Categories</h4>
-        <ul>
-          <li>Oi</li>
-        </ul>
+        {categories.map((category) => (
+          <button type="button" data-testid="category" key={category.id} onClick="função para selecionar a categoria">
+            {category.name}
+          </button>
+        ))}
       </div>
     );
   }
