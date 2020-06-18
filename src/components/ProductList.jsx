ca3c5aeca11f9ff198d26c1195227800836aa6d8
Pedro Pires <pedrotpoliveira@gmail.com>
2020-06-18 10:18:52 -0300

    Initial Commit

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class ProductsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: "",
+      data: []
+    };
+  }
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default ProductsList;
