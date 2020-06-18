b6ee2577a3c930f60d004cf4929859222ba6b745
Pedro Pires <pedrotpoliveira@gmail.com>
2020-06-18 14:12:03 -0300

    Inital Implementation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ProductsList from './components/ProductsList';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      data: [],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <form action="">
+            <i className="fas fa-search" />
+            <input
+              data-testid="query-input"
+              className="SearchBar"
+              type="text"
+            />
+          </form>
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        </header>
+        <section>
+          <ProductsList />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ProductsList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ProductsList() {
+  return <div>hdhdhdh</div>;
+}
+
+export default ProductsList;
