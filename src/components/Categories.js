0ca16eeb73f8328a8b26a58e86cbe0fd9fd197ab
Leandro Marten <leandromarten1@gmail.com>
2020-06-18 15:27:09 -0300

    Basic structure Categories component

---
+++ src/components/Categories.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import * as api from '../services/api'; // importando as funções da API como api
+
+class Categories extends Component {
+
+  render() {
+    return (
+      <div className="categories">
+        <h4>Categories</h4>
+        <ul>
+          <li>Oi</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Categories;
