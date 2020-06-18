fe4de260ef9a0a1607e619953bd417e04fefcfd1
rafaopbar <rafael.coper@outlook.com>
2020-06-18 15:53:38 -0300

    Up

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,27 @@
 export async function getCategories() {
-  // implement here
+  const search = fetch(
+    'https://api.mercadolibre.com/sites/MLB/categories',
+  ).then((data) => data.json());
+  return search;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // implement here
+  const search = fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`,
+  ).then((data) => data.json());
+  return search;
+}
+
+export async function getProductsFromQuery(query) {
+  const search = fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?q=${query}`,
+  ).then((data) => data.json());
+  return search;
+}
+
+export async function getProductsFromCategoryID(categoryID) {
+  const search = fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryID}`,
+  ).then((data) => data.json());
+  return search;
 }
