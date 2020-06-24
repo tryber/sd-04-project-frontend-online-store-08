7286ed94d404068e9b7acb11b5f632275a9a49b5
Leandro Marten <leandromarten1@gmail.com>
2020-06-24 14:45:33 -0300

    Basic Payment

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import '../styles/Payment.css';
 
 class Payment extends Component {
   render() {
-    return <div>Hello from Payment</div>;
+    return (
+      <form className="form-payment">
+        <h4>Método de Pagamento</h4>
+        <label htmlFor="boleto">
+          <input type="radio" name="boleto" value="boleto" />
+          Boleto
+        </label>
+        <label htmlFor="visa">
+          <input type="radio" name="visa" value="visa" />
+          Cartão Crédito Visa
+        </label>
+        <label htmlFor="visa">
+          <input type="radio" name="master" value="master" />
+          Cartão Crédito MasterCard
+        </label>
+        <label htmlFor="visa">
+          <input type="radio" name="elo" value="elo" />
+          Cartão Crédito Elo
+        </label>
+      </form>
+    );
   }
 }
 
